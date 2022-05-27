ca878e073217f9aafb5360059efccf3cf98de089
Victor <abidoye8224@gmail.com>
2022-05-27 18:32:48 +0100

    created Navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
+<template>
+  <Navbar/>
+</template>
+
 <script setup>
+import Navbar from './components/Navbar.vue';
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 </script>
 
-<template>
-  <p>hello World</p>
-</template>
-
-<style>
+<style scoped>
   p {
     color: red;
   }
---
+++ src/components/Navbar.vue
@@ -0,0 +1,10 @@
+<template>
+    <p>{{name}}</p>
+</template>
+<script setup>
+import { ref } from 'vue';
+const name = ref('victor')
+</script>
+<style scoped>
+
+</style>
