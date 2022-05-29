f97bda0cf5b67466ba789d191f88b09569696a0e
Victor <abidoye8224@gmail.com>
2022-05-29 10:55:38 +0100

    Created SectionFive component

--- src/components/SectionFive.vue
+++ src/components/SectionFive.vue
@@ -1,19 +1,36 @@
 <template>
   <div
-    class="h-[927px] md:h-[480px] lg:h-[715px] flex flex-col items-center px-5 md:px-10 lg:px-14 py-5 bg-[#FB8E0B] md:flex-row"
+    class="-z-10 h-[927px] md:h-[480px] lg:h-[715px] items-center px-5 md:px-10 lg:px-14 py-5 bg-[#FB8E0B] flex flex-col justify-around relative"
   >
+    <div class="flex justify-center items-center">
+      <h3
+        class="w-[80%] text-[30px] leading-[40px] lg:text-[48px] lg:leading-[58px] font-bold text-white text-center"
+      >
+        Our blessed client said about us 😍
+      </h3>
+    </div>
     <div class="w-full flex gap-5 flex-col md:flex-row justify-around">
       <div class="flex flex-col items-center">
         <div
-          class="min-w-[343px] max-w-[600px] rounded-md shadow-lg bg-white p-5"
+          class="min-w-[343px] max-w-[600px] md:h-[280px] md:min-w-[auto] lg:h-fit rounded-md shadow-lg bg-white p-5"
         >
-          <p>
+          <h4
+            class="text-[18px] leading-[26px] text-[#FD6003] font-bold pb-3 text-center lg:text-[20px] lg:leading-[28px]"
+          >
+            “Incredible Experience”
+          </h4>
+          <p
+            class="text-[16px] leading-[24px] text-[#383A47] text-center lg:text-[18px] lg:leading-[28px]"
+          >
             We had an incredible experience working with Mixland and were
             impressed they made such a big difference in only three weeks. Our
             team is so grateful for the wonderful improvements they made and
             their ability to get familiar with the concept so quickly.
           </p>
         </div>
+        <div
+          class="-mt-5 mb-5 h-[30px] w-[30px] bg-white rounded-sm rotate-45"
+        ></div>
         <div class="flex text-white gap-x-2">
           <img
             class="h-[62px] w-[62px] object-cover rounded-full"
@@ -28,15 +45,25 @@
       </div>
       <div class="flex flex-col items-center">
         <div
-          class="min-w-[343px] max-w-[600px] rounded-md shadow-lg bg-white p-5"
+          class="min-w-[343px] max-w-[600px] md:h-[280px] md:min-w-[auto] lg:h-fit h-206px rounded-md shadow-lg bg-white p-5"
         >
-          <p>
-            We had an incredible experience working with Mixland and were
-            impressed they made such a big difference in only three weeks. Our
-            team is so grateful for the wonderful improvements they made and
-            their ability to get familiar with the concept so quickly.
+          <h4
+            class="text-[18px] leading-[26px] text-[#FD6003] font-bold pb-3 text-center lg:text-[20px] lg:leading-[28px]"
+          >
+            “Dependable, Responsive, Professional”
+          </h4>
+          <p
+            class="text-[16px] leading-[24px] text-[#383A47] text-center lg:leading-[28px]"
+          >
+            Fermin Apps has collaborated with Mixland team for several projects
+            such as Photo Sharing Apps and Custom Social Networking Apps. The
+            experience has been pleasant, professional and exceeding our
+            expectations. The team is always thinking beyond.
           </p>
         </div>
+        <div
+          class="-mt-5 mb-5 h-[30px] w-[30px] bg-white rounded-sm rotate-45"
+        ></div>
         <div class="flex text-white gap-x-2">
           <img
             class="h-[62px] w-[62px] object-cover rounded-full"
@@ -48,6 +75,15 @@
             <p class="text-[14px] leading-[24px]">CEO, ABC Corporation</p>
           </div>
         </div>
+      </div>
+    </div>
+    <div
+      class="absolute -top-[300px] md:h-[750px] md:w-[750px]  md:-top-[400px] md:-left-[450px]  xl:h-[1000px] xl:w-[1000px] xl:-top-[600px] xl:-left-[450px] -left-[300px] h-[500px] w-[500px] rounded-full border-4 border-white border-opacity-20 flex justify-center items-center"
+    >
+      <div class="h-[400px] w-[400px] md:h-[500px] md:w-[500px] xl:h-[800px] xl:w-[800px] rounded-full border-4 border-white border-opacity-20">
+        <div
+          class="h-[350px] w-[350px] md:h-[400px] md:w-[400px] xl:h-[700px] xl:w-[700px] rounded-full border-4 border-white border-opacity-20"
+        ></div>
       </div>
     </div>
   </div>
