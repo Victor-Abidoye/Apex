92a651c9fc073b797dcaa8e58bafc51f9cedf516
Victor <abidoye8224@gmail.com>
2022-05-29 07:34:23 +0100

    Remove Inbox component and dist folder for TEST purpose

--- src/components/inbox.vue
+++ src/components/inbox.vue
@@ -1,80 +1,110 @@
 <template>
-        <div class="relative">
-            <div class="h-[308px] w-[302px]  lg:h-[407px] lg:w-[416px] shadow-2xl bg-white rounded-lg p-3">
-                <div class="flex gap-x-3 items-center py-4">
-                    <div class="h-[35px] w-[35px] lg:h-[35px] lg:w-[35px] rounded-full bg-[#FFAF63] flex justify-center items-center">
-                        <img src="../assets/inbox.svg" alt="">
-                    </div>
-                    <p class="text-[12px] leading-[17px] lg:text-[16px] lg:leading-[23px]">Inbox</p>
-                    <div class="h-[12px] w-[12px] rounded-full bg-[#C4C4C4] ml-auto">
-
-                    </div>
-                    <div class="h-[12px] w-[12px] rounded-full bg-[#C4C4C4]">
-
-                    </div>
-                    <div class="h-[12px] w-[12px] rounded-full bg-[#C4C4C4]">
-
-                    </div>
-                </div>
-                <div class="bg-[#C4C4C4] h-[223px] lg:h-[308px] rounded-md flex flex-col items-center justify-center gap-2">
-                    <div class= "lg:w-[335px] w-[243px] bg-white rounded-md flex gap-x-3 p-1 lg:p-3">
-                        <div class="h-[35px] w-[35px] bg-[#77F393] rounded-full flex justify-center items-center">
-                            <font-awesome-icon icon="comment"  color="#FFFFFF" />
-                        </div>
-                        <div class="text-[8px] leading-[11px] lg:text-[12px] lg:leading-[16px] ">
-                            <p class="font-bold text-[#12141D] ">New Message</p>
-                            <p class="font-medium text-[#12141D]">Hey Alex, are you free now?</p>
-                        </div>
-                    </div>
-                    <div class= "lg:w-[335px] w-[243px] bg-white rounded-md flex gap-x-3 p-1 lg:p-3">
-                        <div class="h-[35px] w-[35px] bg-[#ED4546] rounded-full flex justify-center items-center">
-                            <font-awesome-icon icon="envelope"  color="#FFFFFF" />
-                        </div>
-                        <div class="text-[8px] leading-[11px] lg:text-[12px] lg:leading-[16px] ">
-                            <p class="font-bold text-[#12141D]">New Message</p>
-                            <p class="font-medium text-[#12141D]">Hey Alex, are you free now?</p>
-                        </div>
-                    </div>
-                    <div class= "lg:w-[335px] w-[243px] bg-white rounded-md flex gap-x-3 p-1 lg:p-3">
-                        <div class="h-[35px] w-[35px] bg-[#008EFF] rounded-full flex justify-center items-center">
-                            <img src="../assets/messanger.svg" alt="">
-                        </div>
-                        <div class="text-[8px] leading-[11px] lg:text-[12px] lg:leading-[16px] ">
-                            <p class="font-bold text-[#12141D]">New Message</p>
-                            <p class="font-medium text-[#12141D]">Hey Alex, are you free now?</p>
-                        </div>
-                    </div>
-                    <div class= "lg:w-[335px] w-[243px] bg-white rounded-md flex gap-x-3 p-1 lg:p-3">
-                        <div class="h-[35px] w-[35px] bg-[#77F393] rounded-full flex justify-center items-center">
-                            <font-awesome-icon icon="comment"  color="#FFFFFF" />
-                        </div>
-                        <div class="text-[8px] leading-[11px] lg:text-[12px] lg:leading-[16px] ">
-                            <p class="font-bold text-[#12141D]">New Message</p>
-                            <p class="font-medium text-[#12141D]">Hey Alex, are you free now?</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <img
-            class="h-[44px] border-4 lg:h-[60px] lg:w-[60px] border-white w-[44px] object-cover rounded-full -mr-4 absolute z-10 top-[30%] -left-[10%]"
-            src="../assets/girl-hat.svg"
-            alt=""
-          />
-            <img
-            class="h-[44px] border-4 border-white lg:h-[60px] lg:w-[60px] w-[44px] object-cover rounded-full -mr-4 absolute top-[57%] -right-[2%] z-10"
-            src="../assets/girl-curl.svg"
-            alt=""
-          />
-            <img
-            class="h-[51px] border-4 lg:h-[60px] lg:w-[60px] border-white w-[51px] object-cover rounded-full -mr-4 absolute top-[92%] left-[10%]"
-            src="../assets/girl-dreads.svg"
-            alt=""
-          />
+  <div class="relative">
+    <div
+      class="h-[308px] w-[302px] lg:h-[407px] lg:w-[416px] shadow-2xl bg-white rounded-lg p-3"
+    >
+      <div class="flex gap-x-3 items-center py-4">
+        <div
+          class="h-[35px] w-[35px] lg:h-[35px] lg:w-[35px] rounded-full bg-[#FFAF63] flex justify-center items-center"
+        >
+          <img src="../assets/inbox.svg" alt="" />
         </div>
+        <p class="text-[12px] leading-[17px] lg:text-[16px] lg:leading-[23px]">
+          Inbox
+        </p>
+        <div class="h-[12px] w-[12px] rounded-full bg-[#C4C4C4] ml-auto"></div>
+        <div class="h-[12px] w-[12px] rounded-full bg-[#C4C4C4]"></div>
+        <div class="h-[12px] w-[12px] rounded-full bg-[#C4C4C4]"></div>
+      </div>
+      <div
+        class="bg-[#C4C4C4] h-[223px] lg:h-[308px] rounded-md flex flex-col items-center justify-center gap-2"
+      >
+        <div
+          class="lg:w-[335px] w-[243px] bg-white rounded-md flex gap-x-3 p-1 lg:p-3"
+        >
+          <div
+            class="h-[35px] w-[35px] bg-[#77F393] rounded-full flex justify-center items-center"
+          >
+            <font-awesome-icon icon="comment" color="#FFFFFF" />
+          </div>
+          <div
+            class="text-[8px] leading-[11px] lg:text-[12px] lg:leading-[16px]"
+          >
+            <p class="font-bold text-[#12141D]">New Message</p>
+            <p class="font-medium text-[#12141D]">
+              Hey Alex, are you free now?
+            </p>
+          </div>
+        </div>
+        <div
+          class="lg:w-[335px] w-[243px] bg-white rounded-md flex gap-x-3 p-1 lg:p-3"
+        >
+          <div
+            class="h-[35px] w-[35px] bg-[#ED4546] rounded-full flex justify-center items-center"
+          >
+            <font-awesome-icon icon="envelope" color="#FFFFFF" />
+          </div>
+          <div
+            class="text-[8px] leading-[11px] lg:text-[12px] lg:leading-[16px]"
+          >
+            <p class="font-bold text-[#12141D]">New Message</p>
+            <p class="font-medium text-[#12141D]">
+              Hey Alex, are you free now?
+            </p>
+          </div>
+        </div>
+        <div
+          class="lg:w-[335px] w-[243px] bg-white rounded-md flex gap-x-3 p-1 lg:p-3"
+        >
+          <div
+            class="h-[35px] w-[35px] bg-[#008EFF] rounded-full flex justify-center items-center"
+          >
+            <img src="../assets/messanger.svg" alt="" />
+          </div>
+          <div
+            class="text-[8px] leading-[11px] lg:text-[12px] lg:leading-[16px]"
+          >
+            <p class="font-bold text-[#12141D]">New Message</p>
+            <p class="font-medium text-[#12141D]">
+              Hey Alex, are you free now?
+            </p>
+          </div>
+        </div>
+        <div
+          class="lg:w-[335px] w-[243px] bg-white rounded-md flex gap-x-3 p-1 lg:p-3"
+        >
+          <div
+            class="h-[35px] w-[35px] bg-[#77F393] rounded-full flex justify-center items-center"
+          >
+            <font-awesome-icon icon="comment" color="#FFFFFF" />
+          </div>
+          <div
+            class="text-[8px] leading-[11px] lg:text-[12px] lg:leading-[16px]"
+          >
+            <p class="font-bold text-[#12141D]">New Message</p>
+            <p class="font-medium text-[#12141D]">
+              Hey Alex, are you free now?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <img
+      class="h-[44px] border-4 lg:h-[60px] lg:w-[60px] border-white w-[44px] object-cover rounded-full -mr-4 absolute z-10 top-[30%] -left-[10%]"
+      src="../assets/girl-hat.svg"
+      alt=""
+    />
+    <img
+      class="h-[44px] border-4 border-white lg:h-[60px] lg:w-[60px] w-[44px] object-cover rounded-full -mr-4 absolute top-[57%] -right-[2%] z-10"
+      src="../assets/girl-curl.svg"
+      alt=""
+    />
+    <img
+      class="h-[51px] border-4 lg:h-[60px] lg:w-[60px] border-white w-[51px] object-cover rounded-full -mr-4 absolute top-[92%] left-[10%]"
+      src="../assets/girl-dreads.svg"
+      alt=""
+    />
+  </div>
 </template>
-<script setup>
-
-</script>
-<style scoped>
-
-</style>
+<script setup></script>
+<style scoped></style>
--- src/components/sectionFour.vue
+++ src/components/sectionFour.vue
@@ -1,38 +1,55 @@
 <template>
-    <div class="h-[748px] md:h-[500px] lg:h-[645px] flex flex-col items-center px-5 md:px-10  lg:px-14 py-5 md:flex-row bg-[#F8F8FA]" >
-
-        <div class=" flex justify-center md:w-1/2">
-            <Inbox/>
-        </div>
-
-        <div class=" h-[60%] flex  flex-col justify-evenly lg:px-6 xl:px-10 md:w-1/2 md:px-6 ">
-            <h2 class="text-[32px] font-bold leading-[40px] text-[#1B1C20] lg:w-[90%] lg:text-[48px] lg:leading-[56px]">Get direct orders from your customers</h2>
-            <p class="font-normal text-[16px] leading-[26px] text-[#383A47] lg:w-[90%]">Create custom landing pages with Rareblocks that converts more visitors than any website. With lots of unique blocks easily build a page. There are many variations of passages of available.  </p>
-            <div class="flex justify-between">
-                <div class="flex">
-                    <p class="text-[32px] leading-[40px] font-medium lg:text-[42px] lg:leading-[56px] text-[#1B1C20]">4.3K+</p>
-                    <div class="flex flex-col text-[14px] leading-[22px] text-[#383A47]">
-                        <p>Website's</p>
-                        <p>Powering</p>
-                    </div>
-                </div>
-                <div class="flex">
-                    <p class="text-[32px] leading-[40px] font-medium lg:text-[42px] lg:leading-[56px]">4.3K+</p>
-                    <div class="flex flex-col text-[14px] leading-[22px]">
-                        <p>Website's</p>
-                        <p>Powering</p>
-                    </div>
-                </div>
-
-            </div>
-        </div>
+  <div
+    class="h-[748px] md:h-[500px] lg:h-[645px] flex flex-col items-center px-5 md:px-10 lg:px-14 py-5 md:flex-row bg-[#F8F8FA]"
+  >
+    <div class="flex justify-center md:w-1/2">
+      <!-- <Inbox /> -->
     </div>
+
+    <div
+      class="h-[60%] flex flex-col justify-evenly lg:px-6 xl:px-10 md:w-1/2 md:px-6"
+    >
+      <h2
+        class="text-[32px] font-bold leading-[40px] text-[#1B1C20] lg:w-[90%] lg:text-[48px] lg:leading-[56px]"
+      >
+        Get direct orders from your customers
+      </h2>
+      <p
+        class="font-normal text-[16px] leading-[26px] text-[#383A47] lg:w-[90%]"
+      >
+        Create custom landing pages with Rareblocks that converts more visitors
+        than any website. With lots of unique blocks easily build a page. There
+        are many variations of passages of available.
+      </p>
+      <div class="flex justify-between">
+        <div class="flex">
+          <p
+            class="text-[32px] leading-[40px] font-medium lg:text-[42px] lg:leading-[56px] text-[#1B1C20]"
+          >
+            4.3K+
+          </p>
+          <div class="flex flex-col text-[14px] leading-[22px] text-[#383A47]">
+            <p>Website's</p>
+            <p>Powering</p>
+          </div>
+        </div>
+        <div class="flex">
+          <p
+            class="text-[32px] leading-[40px] font-medium lg:text-[42px] lg:leading-[56px]"
+          >
+            4.3K+
+          </p>
+          <div class="flex flex-col text-[14px] leading-[22px]">
+            <p>Website's</p>
+            <p>Powering</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import Inbox from './Inbox.vue';
-
+// import Inbox from './Inbox.vue'
 </script>
-<style scoped>
-
-</style>
+<style scoped></style>
